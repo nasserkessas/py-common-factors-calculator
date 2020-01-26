bc1a1f1dda20d2419c7e9afff76ac98f18fe226d
Nasser <nasser@kessas.com>
2020-01-26 18:10:57 +1000

    added input validation

--- comfac.py
+++ comfac.py
@@ -15,10 +15,25 @@ def compare_factors(fac1,fac2,cf):
                 cf.append(i)
     return cf
 
+def ask(question):
+    while True:
+        inp = input(question)
+        try:
+            inp = int(inp)
+            if int(inp) >= 100000: #One hundred thousand
+                print ("This is not a valid input, please write a lower value")
+            elif int(inp) <= 0:
+                print ("This is not a valid input, please write a higher value")
+            else:
+                break
+        except ValueError:
+            print ("This is not a valid input, please write an integer")
+    return inp
 
-num1 = int(input("What is your first number? "))
-num2 = int(input("What is your second number? "))
+num1 = ask(("What is your first number? "))
+num2 = ask(("What is your second number? "))
 find_factors(num1,factors1)
 find_factors(num2,factors2)
 compare_factors(factors1,factors2,com_factors)
-print ('The commom factors of ' + str(num1) + ' ' + str(num2) + ' are ' + str(com_factors))
+commonfactors = ', '.join(str(i) for i in com_factors)
+print ('The common factors of ' + str(num1) + ' and ' + str(num2) + ' are ' + commonfactors)
